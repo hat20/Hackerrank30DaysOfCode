ff152af543159344dd99b4238748430694a5a42e
harshit_tewari <librahtiwari@gmail.com>
2018-02-11 20:29:16 +0530

    Added Day 1 code

---
+++ Day1.py
@@ -0,0 +1,23 @@
+i = 4
+d = 4.0
+s = 'Hackerrank '
+# Declare second integer, double, and String variables.
+j=0
+k=0.0
+m = ''
+# Read and save an integer, double, and String to your variables.
+
+j=int(input())
+k=float(input())
+m=input()
+
+# Print the sum of both integer variables on a new line.
+print(i+j)
+
+# Print the sum of the double variables on a new line.
+print(d+k)
+
+# Concatenate and print the String variables on a new line
+print(s+m)
+
+# The 's' variable above should be printed first.
