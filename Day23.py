84de032168ed73141f9ecee7d407703e6db82061
harshit_tewari <librahtiwari@gmail.com>
2018-03-05 21:05:47 +0530

    added Day 23 code

---
+++ Day23.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        if root != None:
+            q = [root]
+        else:
+            q = []
+            
+        while q:
+            node = q.pop(0)
+            print(node.data, end=" ")
+            
+            if node.left:
+                q.append(node.left)
+                
+            if node.right:
+                q.append(node.right)
+
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
